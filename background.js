f14989e00e07d32d2fab71a9144f8697891068d9
Not-James-Bond <chaitanyasingh92@yahoo.com>
2022-03-22 23:40:58 +0530

    Added Mute Functionality

--- background.js
+++ background.js
@@ -1,12 +1,42 @@
 const SPOTIFY_URL = 'open.spotify.com/';
 const SPOTIFY_AD = 'Spotify – Advertisement';
 
-const random = arr => {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-
 class SpotifyPlayer {
   constructor() {
     this.currentlyPlaying = null;
   }
 }
+
+const isTitleAd = title => {
+  const adRegex = new RegExp(`^${SPOTIFY_AD}`);
+  console.log(`^{SPOTIFY_AD}`.test(title));
+  return !!title.match(adRegex);
+};
+
+const mute = tabId => {
+  chrome.tabs.update(tabId, { muted: true });
+};
+
+const unmute = tabId => {
+  chrome.tabs.update(tabId, { muted: false });
+};
+
+const checkForAd = (tabId, _changeInfo, tab) => {
+  if (tab.url.match(SPOTIFY_URL)) {
+    if (isTitleAd(tab.title)) {
+      if (!tab.mutedInfo.muted) {
+        mute(tabId);
+      }
+    } else {
+      if (tab.mutedInfo.muted) {
+        unmute(tabId);
+      }
+    }
+  }
+};
+
+const run = () => {
+  chrome.tabs.onUpdated.addListener(checkForAd);
+};
+
+run();
